ccd2238efaeb45d3087ad0cec9ef44baa8c27398
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-01 07:14:47 +0000

    new m2e 4.0 project

---
+++ trunk/M2E/M2E/App/Pages/BeforeLogin/Controller/beforeLoginApp.js
@@ -0,0 +1,58 @@
+
+var BeforeLoginApp = angular.module('BeforeLoginApp', ['ngCookies']);
+
+BeforeLoginApp.config(function ($routeProvider) {
+
+    $routeProvider.when("/", { templateUrl: "../../App/Pages/BeforeLogin/Index/Index.html" }).
+                   when("/signup/user/:ref", { templateUrl: "../../App/Pages/BeforeLogin/SignUpUser/SignUpUser.html" }).
+                   when("/signup/client/:ref", { templateUrl: "../../App/Pages/BeforeLogin/SignUpClient/SignUpClient.html" }).
+                   when("/signup/user", { templateUrl: "../../App/Pages/BeforeLogin/SignUpUser/SignUpUser.html" }).
+                   when("/signup/client", { templateUrl: "../../App/Pages/BeforeLogin/SignUpClient/SignUpClient.html" }).
+                   when("/login", { templateUrl: "../../App/Pages/BeforeLogin/Login/Login.html" }).
+                   when("/login/:code", { templateUrl: "../../Resource/templates/beforeLogin/contentView/ajax/signInTemplate.html" }).
+                   when("/facebookLogin/:userType", { templateUrl: "../../Resource/templates/beforeLogin/contentView/facebookLogin.html" }).
+                   when("/facebookLogin", { templateUrl: "../../Resource/templates/beforeLogin/contentView/facebookLogin.html" }).
+                   when("/googleLogin/:userType", { templateUrl: "../../Resource/templates/beforeLogin/contentView/googleLogin.html" }).
+                   when("/googleLogin", { templateUrl: "../../Resource/templates/beforeLogin/contentView/googleLogin.html" }).
+                   when("/linkedinLogin/:userType", { templateUrl: "../../Resource/templates/beforeLogin/contentView/linkedinLogin.html" }).
+                   when("/linkedinLogin", { templateUrl: "../../Resource/templates/beforeLogin/contentView/linkedinLogin.html" }).
+                   when("/validate/:userName/:guid", { templateUrl: "../../Resource/templates/beforeLogin/contentView/validateAccount.html" }).
+                   when("/tnc", { templateUrl: "../../Resource/templates/beforeLogin/contentView/termsAndConditions.html" }).
+                   when("/404", { templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" }).
+                   when("/signup", { templateUrl: "../../Resource/templates/beforeLogin/contentView/signup.html" }).
+                   when("/forgetpassword", { templateUrl: "../../Resource/templates/beforeLogin/contentView/forgetPassword.html" }).
+                   otherwise({ templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" });
+
+});
+
+BeforeLoginApp.run(function ($rootScope, $location) { //Insert in the function definition the dependencies you need.
+    
+    $rootScope.$on("$locationChangeStart",function(event, next, current){
+        //Do your things        
+        //var path = $location.path();
+        gaWeb("BeforeLogin-Page Visited", "Page Visited", next);
+        var path = next.split('#');        
+        gaPageView(path,'title');            
+    });
+});
+
+
+function loadjscssfile(filename, filetype) {
+    var fileref = "";
+    if (filetype == "js") { //if filename is a external JavaScript file
+        fileref = document.createElement('script');
+        fileref.setAttribute("type", "text/javascript");
+        fileref.setAttribute("src", filename);
+    }
+    else if (filetype == "css") { //if filename is an external CSS file
+        fileref = document.createElement("link");
+        fileref.setAttribute("rel", "stylesheet");
+        fileref.setAttribute("type", "text/css");
+        fileref.setAttribute("href", filename);
+    }
+    if (typeof fileref != "undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref);
+}
+
+//loadjscssfile("../../App/Pages/BeforeLogin/SignUpClient/signUpClientController.js", "js"); //dynamically load and add this .js file
+//loadjscssfile("../../App/Pages/BeforeLogin/Controller/common/CookieService.js", "js"); 
