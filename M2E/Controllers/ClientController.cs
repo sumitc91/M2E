ee8f7f411edce41b15dcc384c3b7ff6a4c90a498
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-07-02 15:50:01 +0000

    backend google analytics.

--- M2E/Controllers/ClientController.cs
+++ M2E/Controllers/ClientController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using M2E.Common.Logger;
+using System.Reflection;
+using System.Configuration;
 
 namespace M2E.Controllers
 {
@@ -10,9 +13,11 @@ namespace M2E.Controllers
     {
         //
         // GET: /Client/
+        private ILogger logger = new Logger(Convert.ToString(MethodBase.GetCurrentMethod().DeclaringType));
 
         public ActionResult Index()
         {
+            logger.Info("Client Controller index page");  
             return View();
         }
 
