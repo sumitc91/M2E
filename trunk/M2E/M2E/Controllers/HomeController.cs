b07501a72c871e6e57c0314d7804a566070ae42d
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-02 10:19:06 +0000

    log4net.dll implemented.

--- trunk/M2E/M2E/Controllers/HomeController.cs
+++ trunk/M2E/M2E/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Reflection;
+using M2E.Common.Logger;
 
 namespace M2E.Controllers
 {
@@ -11,8 +13,11 @@ namespace M2E.Controllers
         //
         // GET: /Home/
 
+        private ILogger logger = new Logger(Convert.ToString(MethodBase.GetCurrentMethod().DeclaringType));
+
         public ActionResult Index()
         {
+            logger.Info("index page");
             return View();
         }
 
