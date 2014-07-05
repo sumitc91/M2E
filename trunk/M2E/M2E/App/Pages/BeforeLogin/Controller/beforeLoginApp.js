8ae6c75c6557083e64996f97a2470c48db50baf2
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-05 11:30:27 +0000

    ContactUs Added

--- trunk/M2E/M2E/App/Pages/BeforeLogin/Controller/beforeLoginApp.js
+++ trunk/M2E/M2E/App/Pages/BeforeLogin/Controller/beforeLoginApp.js
@@ -20,6 +20,7 @@ BeforeLoginApp.config(function ($routeProvider) {
                    when("/tnc", { templateUrl: "../../Resource/templates/beforeLogin/contentView/termsAndConditions.html" }).
                    when("/404", { templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" }).
                    when("/AboutUs", { templateUrl: "../../App/Pages/BeforeLogin/AboutUs/AboutUs.html" }).
+                   when("/contactus", { templateUrl: "../../App/Pages/BeforeLogin/ContactUs/contactus.html" }).
                    when("/forgetpassword", { templateUrl: "../../Resource/templates/beforeLogin/contentView/forgetPassword.html" }).
                    otherwise({ templateUrl: "../../Resource/templates/beforeLogin/contentView/404.html" });
 
