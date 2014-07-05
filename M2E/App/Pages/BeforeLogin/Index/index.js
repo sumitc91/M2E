9643078567ffa347f971ca84c282abd18e96a60d
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-05 04:47:47 +0000

    Google Analytics at Footer

--- M2E/App/Pages/BeforeLogin/Index/index.js
+++ M2E/App/Pages/BeforeLogin/Index/index.js
@@ -1,4 +1,7 @@
 BeforeLoginApp.controller('beforeLoginIndex', function ($scope, $http, $rootScope, CookieUtil) {
     $scope.logoImage = { url: logoImage };
     $('title').html("MadeToEarn");
+    $scope.beforeLoginFooterCopyRightInfo = {
+        companyName: "MadeToEarn"
+    };
 });
