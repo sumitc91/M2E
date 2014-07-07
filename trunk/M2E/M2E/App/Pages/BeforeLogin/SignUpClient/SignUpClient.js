df605562fe84561eb87a0c3fb976837e606df5ec
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-07 09:26:17 +0000

    ajax loading changes.

--- trunk/M2E/M2E/App/Pages/BeforeLogin/SignUpClient/SignUpClient.js
+++ trunk/M2E/M2E/App/Pages/BeforeLogin/SignUpClient/SignUpClient.js
@@ -6,55 +6,61 @@ BeforeLoginApp.controller('signUpClientController', function ($scope, $http, $ro
         EmailId: "",
         Password: "",
         ConfirmPassword: "",
-        CompanyName:""
-    };    
+        CompanyName: ""
+    };
     $('#client_signup_company_textBox_id').hide();
     $("[name='sliding_client_company_checkbox']").bootstrapSwitch();
-    $('input[name="sliding_client_company_checkbox"]').on({        
-        'switchChange.bootstrapSwitch': function (event, state) {                        
+    $('input[name="sliding_client_company_checkbox"]').on({
+        'switchChange.bootstrapSwitch': function (event, state) {
             if ($(this).is(':checked')) {
-                $('#client_signup_company_textBox_id').show();                
+                $('#client_signup_company_textBox_id').show();
             } else {
-                $('#client_signup_company_textBox_id').hide();                
-            }            
+                $('#client_signup_company_textBox_id').hide();
+            }
         }
     });
-    
+
     $scope.ClientSignUp = function () {
         var checkUserExistsRequestData = {
             userName: 'test'  //need to pass from the calling methods.
-        }        
-        var url = ServerContextPah + '/Auth/Login/web';                        
-        $.blockUI({
-            message: '<h1><img src="../../App/img/loading/loading123.gif" /></h1>',
-            css: {
-                border: 'none',
-                padding: '15px',
-                backgroundColor: '#000',
-                '-webkit-border-radius': '10px',
-                '-moz-border-radius': '10px',
-                opacity: .5,
-                color: '#fff'
-            }
-        });
-        //$.blockUI({ message: '<h1><img src="../../App/img/loading/loading123.gif" /></h1>' });
+        }
+        var url = ServerContextPah + '/Auth/Login/web';
+
+        startBlockUI('wait..',3);
         $http({
             url: url,
             method: "POST",
             headers: { 'Content-Type': 'application/json' }
         }).success(function (data, status, headers, config) {
             //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
-            $.unblockUI();
-            
+            stopBlockUI();
+
         }).error(function (data, status, headers, config) {
-            
+
         });
     }
-    
+
 
 });
 
+function startBlockUI(mssg,size) {
+    $.blockUI({
+        message: '<h' + size + '><img src="../../App/img/loading/loading123.gif" />' + mssg + '</h' + size + '>',
+        css: {
+            border: 'none',
+            padding: '15px',
+            backgroundColor: '#000',
+            '-webkit-border-radius': '10px',
+            '-moz-border-radius': '10px',
+            opacity: .5,
+            color: '#fff'
+        }
+    });
+}
 
+function stopBlockUI() {
+    $.unblockUI();
+}
 
 function isValidEmailAddress(emailAddress) {
     var pattern = new RegExp(/^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i);
