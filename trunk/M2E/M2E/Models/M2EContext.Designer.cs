1ef4cc2d72a264d0c2f8a1516c0709beb724594b
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-07 16:59:34 +0000

    edmx modified

--- trunk/M2E/M2E/Models/M2EContext.Designer.cs
+++ trunk/M2E/M2E/Models/M2EContext.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'F:\PcOnGo_SVN\trunk\M2E\M2E\Models\M2E.edmx'. 
+﻿// Default code generation is disabled for model 'E:\pcongo_svn\pcongo\trunk\M2E\M2E\Models\M2EContext.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
