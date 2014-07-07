f168ff57c552fda1b793ec426ca1c06992611cda
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-07 12:09:51 +0000

    client sign up updates.

--- trunk/M2E/M2E/Controllers/AuthController.cs
+++ trunk/M2E/M2E/Controllers/AuthController.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using M2E.Models.DataWrapper;
 
 namespace M2E.Controllers
 {
     public class AuthController : Controller
     {
         //
-        // GET: /Auth/
+        // GET: /Auth/        
 
         public ActionResult Index()
         {
@@ -23,6 +24,17 @@ namespace M2E.Controllers
             var returnUrl = "/";
             var referral = Request.QueryString["ref"];
             var response = new ResponseModel<string> {Status = 200, Message = "success", Payload = "1234567890"};
+            System.Threading.Thread.Sleep((5000));
+            return Json(response);
+        }
+
+        [HttpPost]
+        public JsonResult CreateAccount(ClientRegisterationRequest req)
+        {
+            
+            var returnUrl = "/";
+            var referral = Request.QueryString["ref"];
+            var response = new ResponseModel<string> { Status = 200, Message = "success", Payload = "1234567890" };
             System.Threading.Thread.Sleep((5000));
             return Json(response);
         }
---
+++ trunk/M2E/M2E/Models/DataWrapper/ClientRegisterationRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataWrapper
+{
+    public class ClientRegisterationRequest
+    {    
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string CompanyName { get; set; }
+        public string Type { get; set; }
+    }
+}
