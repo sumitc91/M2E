f4edd1d9c165096ca3f49fb76047f488d58fcb40
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-10 15:08:01 +0000

    User Sign UP

---
+++ M2E/Models/DataWrapper/UserRegisterationRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataWrapper
+{
+    public class UserRegisterationRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Type { get; set; }
+        public string Source { get; set; }
+        public string Referral { get; set; }
+    }
+}
