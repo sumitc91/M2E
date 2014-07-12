3e898d85bef1ead1e3bbee5f98d7b4340685ca37
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-12 03:43:35 +0000

    General RegisterationRequest.cs

--- trunk/M2E/M2E/Models/DataWrapper/RegisterationRequest.cs
+++ trunk/M2E/M2E/Models/DataWrapper/RegisterationRequest.cs
@@ -5,12 +5,13 @@ using System.Web;
 
 namespace M2E.Models.DataWrapper
 {
-    public class UserRegisterationRequest
-    {
+    public class RegisterationRequest
+    {    
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string CompanyName { get; set; }
         public string Type { get; set; }
         public string Source { get; set; }
         public string Referral { get; set; }
