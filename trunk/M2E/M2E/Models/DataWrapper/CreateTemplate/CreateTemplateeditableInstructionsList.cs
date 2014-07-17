4081a4518b220289a79475a740107da69e5d8bd7
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-17 08:23:46 +0000

    CreateTemplate code behind model added.

---
+++ trunk/M2E/M2E/Models/DataWrapper/CreateTemplate/CreateTemplateeditableInstructionsList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace M2E.Models.DataWrapper.CreateTemplate
+{
+    public class CreateTemplateeditableInstructionsList
+    {
+        public string Number { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ trunk/M2E/M2E/Models/DataWrapper/CreateTemplateRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using M2E.Models.DataWrapper.CreateTemplate;
+
+namespace M2E.Models.DataWrapper
+{
+    public class CreateTemplateRequest
+    {
+        public List<CreateTemplateQuestionInfo> Data { get; set; }
+    }    
+}
