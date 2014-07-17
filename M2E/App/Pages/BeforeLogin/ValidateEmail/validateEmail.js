7e28da39ba3e918ec220de90a3a7f34e664ce955
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-17 07:24:20 +0000

    Functionality is added in ValidateAccount

--- M2E/App/Pages/BeforeLogin/ValidateEmail/validateEmail.js
+++ M2E/App/Pages/BeforeLogin/ValidateEmail/validateEmail.js
@@ -3,6 +3,19 @@ BeforeLoginApp.controller('validateEmailTemplate', function ($scope, $http, $rou
         userName: $routeParams.userName,
         guid: $routeParams.guid
     };
+    $scope.Header = {
+        message: '',
+        className: '',
+        iconClassName: ''
+    };
+    $scope.Content = {
+        header1: '',
+        header2: '',
+        contentClasstheme: '',
+        header2IconClassName: '',
+        message: '',
+        companyName: ''
+    };
     $http({
         url: '/Auth/ValidateAccount',
         method: "POST",
@@ -11,10 +24,36 @@ BeforeLoginApp.controller('validateEmailTemplate', function ($scope, $http, $rou
     }).success(function (data, status, headers, config) {
         //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
         if (data.Status == "200") {
-            alert("successfully validated");
+            $scope.Header.message = "Account Validated";
+            $scope.Header.className = "alert-success";
+            $scope.Header.iconClassName = "fa-check";
+            $scope.Content.header1 = "Welcome to";
+            $scope.Content.companyName = "MadeToEarn";
+            $scope.Content.contentClasstheme = "callout-info";
+            $scope.Content.header2IconClassName = "fa fa-info";
+            $scope.Content.header2 = "Thanks for choosing MadeToEarn.";
+            $scope.Content.message = "Your Account is successfully verified.";
         }
         else if (data.Status == "500") {
-            alert("internal server error");
+            $scope.Header.message = "Internal Error";
+            $scope.Header.className = "alert-danger";
+            $scope.Header.iconClassName = "fa-ban";
+            $scope.Content.header1 = "500";
+            $scope.Content.companyName = "MadeToEarn";
+            $scope.Content.contentClasstheme = "callout-danger";
+            $scope.Content.header2IconClassName = "fa fa-warning text-yellow";
+            $scope.Content.header2 = "Oops! Something went wrong.";
+            $scope.Content.message = "We could not find the page you were looking for.";
+        } else {
+            $scope.Header.message = "Link Expired";
+            $scope.Header.className = "alert-warning";
+            $scope.Header.iconClassName = "fa-warning";
+            $scope.Content.header1 = "";
+            $scope.Content.companyName = "";
+            $scope.Content.contentClasstheme = "callout-warning";
+            $scope.Content.header2IconClassName = "fa fa-warning text-yellow";
+            $scope.Content.header2 = "Oops! You are trying to access expired link.";
+            $scope.Content.message = "You have already used the link to validate your account.";
         }
         console.log(data);
     }).error(function (data, status, headers, config) {
