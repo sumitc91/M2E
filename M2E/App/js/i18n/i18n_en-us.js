d14e132aae7abeab3c171eb227ac1e91ecbe04ce
Sumit Chourasia <sumitchourasia91@gmail.com>
2014-07-18 09:05:26 +0000

    internationalization hindi text added.

--- M2E/App/js/i18n/i18n_en-us.js
+++ M2E/App/js/i18n/i18n_en-us.js
@@ -1,5 +1,24 @@
 ﻿window.madetoearn = window.madetoearn || {};
 window.madetoearn.i18n = {
     //signin page.
-    "signInTitle": "SIGN IN"
+    "signInTitle": "SIGN IN",
+
+    // before login master page
+    "beforeLoginCompanyTitle":"Made To Earn",
+    "beforeLoginMasterPageAboutM2E": "What is M2E?",
+    "beforeLoginMasterPageSignIn": "Sign In",
+    "beforeLoginMasterPageContactUs": "Contact Us",
+    "beforeLoginMasterPageFAQ": "FAQ",
+    "beforeLoginMasterPageTnC": "Terms & Privacy",
+
+    //beflore login index page
+    "beforeLoginIndexPageM2EText": "Made To Earn",
+    "beforeLoginIndexPageClientMoreInfo": "More Info",
+    "beforeLoginIndexPageClientAbout": "About",
+    "beforeLoginIndexPageUserMoreInfo": "More Info",
+    "beforeLoginIndexPageUserRegister": "Register",
+    "beforeLoginIndexPageUserText": "User",
+    "beforeLoginIndexClientText": "Client",
+    "beforeLoginIndexPageClientHireText": "HIRE",
+    "beforeLoginIndexPageClientWorkText": "Work"
 };
--- M2E/App/js/i18n/i18n_hi_in.js
+++ M2E/App/js/i18n/i18n_hi_in.js
@@ -1,5 +1,24 @@
 ﻿window.madetoearn = window.madetoearn || {};
 window.madetoearn.i18n = {
     //signin page.
-    "signInTitle": "लॉगिन"
+    "signInTitle": "लॉगिन",
+
+    // before login master page
+    "beforeLoginCompanyTitle": "बिंदास कमाएँ",
+    "beforeLoginMasterPageAboutM2E": "M2E क्या है?",
+    "beforeLoginMasterPageSignIn": "साइन इन",
+    "beforeLoginMasterPageContactUs": "हमसे संपर्क करें",
+    "beforeLoginMasterPageFAQ": "पूछे जाने वाले प्रश्न",
+    "beforeLoginMasterPageTnC": "शर्तें और गोपनीयता",
+
+    //beflore login index page
+    "beforeLoginIndexPageM2EText": "बिंदास कमाएँ",
+    "beforeLoginIndexPageClientMoreInfo": "अधिक जानकारी",
+    "beforeLoginIndexPageClientAbout": "रजिस्टर",
+    "beforeLoginIndexPageUserMoreInfo": "अधिक जानकारी",
+    "beforeLoginIndexPageUserAbout": "रजिस्टर",
+    "beforeLoginIndexPageUserText": "उपयोगकर्ता",
+    "beforeLoginIndexClientText": "ग्राहक",
+    "beforeLoginIndexPageClientHireText": "किराये पर",
+    "beforeLoginIndexPageClientWorkText": "काम करना"
 };
