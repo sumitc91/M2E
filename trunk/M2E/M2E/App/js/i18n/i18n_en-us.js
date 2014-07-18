fc3605e3832afc38c4b273cae3c81d360ff5b1dc
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-18 10:03:28 +0000

    internationalization hindi text added.

--- trunk/M2E/M2E/App/js/i18n/i18n_en-us.js
+++ trunk/M2E/M2E/App/js/i18n/i18n_en-us.js
@@ -20,5 +20,10 @@ window.madetoearn.i18n = {
     "beforeLoginIndexPageUserText": "User",
     "beforeLoginIndexClientText": "Client",
     "beforeLoginIndexPageClientHireText": "HIRE",
-    "beforeLoginIndexPageClientWorkText": "Work"
+    "beforeLoginIndexPageClientWorkText": "Work",
+    "beforeLoginIndexPageClientIneedItDone": "I need it done",
+    "beforeLoginIndexPageRegisterAsClient": "Register as Client",
+    "beforeLoginIndexPageUserIWillDoIt": "I will do it",
+    "beforeLoginIndexPageRegisterAsUser": "Register as User",
+    "beforeLoginIndexPageAboutM2EShordDescpription": "Made2earn is India's first technological initiative that aims at utilizing the potential of the millions of Indian youth and provide them with financial Independence. Our goal is to provide all Indian University attending students a better value for their time. All the jobs will be of less than 15min. For our clients it an opportunity to harness the potential of the general public. Human intelligence is at stake here. Market surveys, data entry, captcha reading, opinion polls, product testing, social media marketing, app testing, video marketing, online advertisements and the list goes on to anything imaginable. These are all done at a fraction of cost as compared to other freelancing companies."
 };
--- trunk/M2E/M2E/App/js/i18n/i18n_hi_in.js
+++ trunk/M2E/M2E/App/js/i18n/i18n_hi_in.js
@@ -20,5 +20,10 @@ window.madetoearn.i18n = {
     "beforeLoginIndexPageUserText": "उपयोगकर्ता",
     "beforeLoginIndexClientText": "ग्राहक",
     "beforeLoginIndexPageClientHireText": "किराये पर",
-    "beforeLoginIndexPageClientWorkText": "काम करना"
+    "beforeLoginIndexPageClientWorkText": "काम करना",
+    "beforeLoginIndexPageClientIneedItDone": "मुझे काम पूरा",
+    "beforeLoginIndexPageRegisterAsClient": "क्लाइंट के रूप में पंजीकरण",
+    "beforeLoginIndexPageUserIWillDoIt": "मैं यह करूँगा",
+    "beforeLoginIndexPageRegisterAsUser": "उपयोगकर्ता के रूप में पंजीकरण",
+    "beforeLoginIndexPageAboutM2EShordDescpription": "Made2earn भारतीय युवाओं के लाखों लोगों की क्षमता का उपयोग करना है और वित्तीय स्वतंत्रता के साथ उन्हें प्रदान करते हैं कि भारत के पहले तकनीकी पहल है. हमारा लक्ष्य सभी भारतीय विश्वविद्यालय में भाग लेने के लिए छात्रों को अपने समय के लिए एक बेहतर मूल्य प्रदान करने के लिए है. सभी कार्य 15min से कम का हो जाएगा. हमारे ग्राहकों के लिए यह एक अवसर के आम जनता की क्षमता का दोहन करने के लिए. मानव खुफिया यहां दांव पर लगी है. बाजार सर्वेक्षण, डेटा प्रविष्टि, कैप्चा पढ़ने, जनमत सर्वेक्षणों, उत्पाद परीक्षण, सामाजिक मीडिया विपणन, app परीक्षण, वीडियो विपणन, ऑनलाइन विज्ञापन और सूची कल्पनीय कुछ भी करने के लिए चला जाता है. अन्य स्वतंत्र कंपनियों की तुलना में इन सभी लागत का एक अंश पर किया जाता है."
 };
