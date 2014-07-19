bfbd2694bad45ed2d58761ac81d6c6aa44756b25
dgchanchal <dgchanchal@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-19 09:15:45 +0000

    Login.html

--- trunk/M2E/M2E/App/Pages/BeforeLogin/Login/Login.js
+++ trunk/M2E/M2E/App/Pages/BeforeLogin/Login/Login.js
@@ -1,6 +1,24 @@
 BeforeLoginApp.controller('beforeLoginSignInController', function ($scope, $http, $rootScope, CookieUtil) {
     $scope.EmailId = "";
     $scope.Password = "";
+    $scope.showHeaderErrors = false;
+    $scope.showFooterErrors = false;
+    $scope.EmailIdAlert = {
+        visible: false,
+        message: ''
+    };
+    $scope.PasswordAlert = {
+        visible: false,
+        message: ''
+    };
+    $scope.HeaderAlert = {
+        visible: false,
+        message: ''
+    };
+    $scope.ForgetPasswordAlert = {
+        visible: false,
+        message: ''
+}
 
     $scope.Login = function () {
 
@@ -11,8 +29,27 @@ BeforeLoginApp.controller('beforeLoginSignInController', function ($scope, $http
             KeepMeSignedInCheckBox: 'checked',            
         }
         var url = ServerContextPah + '/Auth/Login';
-        
+        var validatePassword = false;
+
         if (isValidEmailAddress($scope.EmailId)) {
+            $scope.EmailIdAlert.visible = false;
+            $scope.EmailIdAlert.message = "";
+        }
+        else {
+            $scope.EmailIdAlert.visible = true;
+            $scope.EmailIdAlert.message = "Entered emil id is invalid. Please enter valid email id.";
+        }
+        if ($scope.Password != "") {
+            validatePassword = true;
+            $scope.PasswordAlert.visible = false;
+            $scope.PasswordAlert.message = "";
+        }
+        else {
+            $scope.PasswordAlert.visible = true;
+            $scope.PasswordAlert.message = "Password cannot be empty !!!";
+        }
+        
+        if (isValidEmailAddress($scope.EmailId) && validatePassword) {
             startBlockUI('wait..', 3);
             $http({
                 url: url,
@@ -22,12 +59,26 @@ BeforeLoginApp.controller('beforeLoginSignInController', function ($scope, $http
             }).success(function (data, status, headers, config) {
                 //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
                 stopBlockUI();
-                if (data.Status == "401")
+                if (data.Status == "401") {
                     showToastMessage("Notice", "The username/password combination is incorrect !");
-                else if (data.Status == "500")
+                    $scope.showHeaderErrors = true;
+                    $scope.HeaderAlert.visible = true;
+                    $scope.HeaderAlert.message = "The username/password combination you entered is incorrect. Please try again(make sure your caps lock is off).";
+                    $scope.ForgetPasswordAlert.visible = true;
+                    $scope.ForgetPasswordAlert.message = "Forgot your password?";
+                }
+                else if (data.Status == "500") {
+                    $scope.showHeaderErrors = true;
+                    $scope.HeaderAlert.visible = true;
+                    $scope.HeaderAlert.message = "Internal server error occured. Please try again.";
                     showToastMessage("Error", "Internal Server Error Occured !");
-                else if (data.Status == "403")
+                }
+                else if (data.Status == "403") {
                     showToastMessage("Warning", "Your Account is not verified. Please check your mail !");
+                    $scope.showHeaderErrors = true;
+                    $scope.HeaderAlert.visible = true;
+                    $scope.HeaderAlert.message = "Your Account is not verified yet. Please check your mail and verfiy your account.";
+                }
                 else if (data.Status == "200") {
                     showToastMessage("Success", "Successfully Logged in !");
                     console.log("data : " + data);
@@ -39,7 +90,7 @@ BeforeLoginApp.controller('beforeLoginSignInController', function ($scope, $http
             });
         }
         else {
-            $scope.showErrors = true;
+            $scope.showFooterErrors = true;
             showToastMessage("Error", "Some Fields are Invalid !!!");
         }
 
