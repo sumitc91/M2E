66c13ce467456bc60346c3660e64ae0b49154786
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-19 17:40:26 +0000

    client view angular js

--- trunk/M2E/M2E/App/Pages/ClientAfterLogin/Index/index.js
+++ trunk/M2E/M2E/App/Pages/ClientAfterLogin/Index/index.js
@@ -1,4 +1,11 @@
 ClientAfterLoginApp.controller('ClientAfterLoginIndex', function ($scope, $http, $rootScope, CookieUtil) {
     $scope.logoImage = { url: logoImage };  
     $('title').html("Client - MadeToEarn");
+
+    $scope.InProgressTaskList = [{ showEllipse: true, title: "my first template", timeShowType: "info", showTime: "5 hours", editId: "", creationDate: "an 2014" },
+        { showEllipse: true, title: "my second template", timeShowType: "danger", showTime: "2 hours", editId: "", creationDate: "feb 2013" },
+        { showEllipse: true, title: "my third template", timeShowType: "warning", showTime: "1 day", editId: "", creationDate: "march 3023" },
+        { showEllipse: true, title: "my fourth template", timeShowType: "success", showTime: "3 days", editId: "", creationDate: "aug 1203" },
+        { showEllipse: true, title: "my fifth template", timeShowType: "default", showTime: "5 hours", editId: "", creationDate: "nov 2015" }
+    ];
 });
