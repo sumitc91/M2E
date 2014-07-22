4e47b3337ea15f0937a49d0a07b2aa1c20cab58b
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-22 10:28:45 +0000

    image uploaded enhancement in edit template.

--- trunk/M2E/M2E/Models/DataResponse/ImgurImageResponse.cs
+++ trunk/M2E/M2E/Models/DataResponse/ImgurImageResponse.cs
@@ -12,6 +12,7 @@ namespace M2E.Models.DataResponse
     public class imgurData
     {
         public string id { get; set; }
+        public string copyText { get; set; }
         public string deletehash { get; set; }
         public string link { get; set; }
         public string link_s { get; set; }
