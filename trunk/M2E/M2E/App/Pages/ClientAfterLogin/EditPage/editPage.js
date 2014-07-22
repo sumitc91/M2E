d4d64a68f2c8afd5b55ac77fc08ba01a3b0e86c0
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-22 09:35:02 +0000

    client detail show implemented

--- trunk/M2E/M2E/App/Pages/ClientAfterLogin/EditPage/editPage.js
+++ trunk/M2E/M2E/App/Pages/ClientAfterLogin/EditPage/editPage.js
@@ -1,3 +1,30 @@
-ClientAfterLoginApp.controller('ClientAfterLoginEditPage', function ($scope, $http, $rootScope, CookieUtil) {    
+ClientAfterLoginApp.controller('ClientAfterLoginEditPage', function ($scope, $http, $rootScope, CookieUtil) {
     $('title').html("Client - MadeToEarn");
+    loadClientDetails();
+
+    function loadClientDetails() {
+        var url = ServerContextPah + '/Client/GetClientDetails?username=' + userSession.username;
+        startBlockUI('wait..', 3);
+        $http({
+            url: url,
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' }
+        }).success(function (data, status, headers, config) {
+            //$scope.persons = data; // assign  $scope.persons here as promise is resolved here
+            stopBlockUI();
+            if (data.Status == "200") {
+                $rootScope.clientDetailResponse = data.Payload;
+            }
+            else if (data.Status == "404") {
+
+                alert("This template is not present in database");
+            }
+            else if (data.Status == "500") {
+
+                alert("Internal Server Error Occured");
+            }
+        }).error(function (data, status, headers, config) {
+
+        });
+    }
 });
