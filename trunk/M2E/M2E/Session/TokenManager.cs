ed5d69a0ee5459629e89db1e5a6ea34cf03e09ba
sumitc91 <sumitc91@26c8ff76-45bc-40fb-aa89-bc4668cafcb9>
2014-07-23 06:33:37 +0000

    Session added in the project

---
+++ trunk/M2E/M2E/Session/TokenManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Caching;
+
+namespace M2E.Session
+{
+    public class TokenManager
+    {
+        public static void CreateSession(M2ESession session)
+        {
+            string sessionId = session.SessionId;
+            int hours = 1; // TODO: currently hard coded hour value;
+            MemoryCache.Default.Set(sessionId, session, new CacheItemPolicy() { SlidingExpiration = new TimeSpan(hours, 0, 0) });
+        }
+
+        public static void removeSession(string sessionId)
+        {
+            MemoryCache.Default.Remove(sessionId);
+        }
+
+        public static bool isValidSession(string sessionId)
+        {
+            M2ESession session = null;
+            return isValidSession(sessionId, out session);
+        }
+
+        private static bool isValidSession(string sessionId, out  M2ESession session)
+        {
+            session = null;
+            if (MemoryCache.Default.Contains(sessionId))
+            {
+                session = (M2ESession)MemoryCache.Default.Get(sessionId);
+            }
+            return verifySessionObject(session);
+        }
+
+        private static bool verifySessionObject(M2ESession session)
+        {
+            return session != null;
+        }
+    }
+}
